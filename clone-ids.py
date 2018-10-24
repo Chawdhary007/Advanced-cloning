34581a1815fa8062bb235e156c86831257b2cf93
chawdhary <44389314+chawdhary@users.noreply.github.com>
2018-10-24 19:54:53 +0500

    Update clone-ids.py

--- clone-ids.py
+++ clone-ids.py
@@ -296,7 +296,7 @@ def crack0(data,sandi,p):
 			tampil('\rh==>\rk%s \rm[\rp%s\rm]'%(i,sandi))
 	tampil('\rh[*]nmbr of clone accounts\rp>>   %d'%len(akun_sukses))
 	tampil('\rm[*]nmbr of total accounts\rp>>   %d'%len(akun_gagal))
-	tampil('\rk[*]nmbr of extra accounts>>      %d'%len(akun_cekpoint))
+	tampil('\rk[*]nmbr of extra accounts>>    %d'%len(akun_cekpoint))
 	tampil('\rc[*]nmbr of error ids\rp>>        %d'%len(akun_error))
 	if p:
 		i = inputD('[?]No id found want try again (y/n)',['Y','N'])
